b6a7622eda7ac459583225b0dd92203ef8d2bcb1
Joslyn <jrosbrook@gmail.com>
2014-02-16 16:50:07 -0800

    updated readme/test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 An exercise in writing own merge sort in Ruby (v 2.1)
+
+It is a comparison-based sorting algorith that recursively divides an array, compares the parts, and builds a new array up from least to greatest. 
+
+Talking to Lillian and other classmates helped me figure out the difference in sorts. 
--- test/merge-sort_test.rb
+++ test/merge-sort_test.rb
@@ -3,7 +3,8 @@ require "merge-sort"
 
 describe "use merge_sort method" do
   it "sorts array from least to greatest" do
-    array = []
-    MergeSort.new.sort(array).must_equal []
+    array = [6,33,42,8,100,13, 66, -85, 0.8]
+    sorted_array = merge(array)
+    sorted_array.must_equal [-85, 0.8, 6, 8, 13, 33, 42, 66, 100]
   end
 end
