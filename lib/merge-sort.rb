b314fe52b99d2d69ed0cf96a6cf8fc4a716be97e
Joslyn <jrosbrook@gmail.com>
2014-02-16 16:15:06 -0800

    logic

--- lib/merge-sort.rb
+++ lib/merge-sort.rb
@@ -1,1 +1,36 @@
-def 
+def  merge_sort(array)
+  return array if array.count <= 1
+
+  middle = array.count/2
+  left = array.slice(0, middle)
+  right = array.slice(middle, array.count - middle)
+
+  array = []
+  offset_l = 0
+  offset_r = 0
+  while offset_l < part_l.count && offset_r < part_r.count
+    l = part_l[offset_l]
+    r = part_r[offset_r]
+
+    if l <= r   
+      array << l
+      offset_l += 1
+    else 
+      array << r 
+      offset_r += 1
+    end
+  end 
+
+  while offset_l < part_l.count
+    array << part_l[offset_l]
+    offset_l += 1
+  end
+
+  while offset_r < part_r.count
+    array << part_r[offset_r]
+    offset_r += 1
+  end
+
+  return array
+
+end 
